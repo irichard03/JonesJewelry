8bd0d831fe419cf9a2caa8dae4b44b0a907e891c
Ian Richard <ianrichard03@gmail.com>
2019-06-10 21:41:37 -0500

    restyled iphone page.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Container">
-          <nav className="NavBar">
-            <ul>
-              <li>About</li>
-              <li>Gallery</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-  
-          <section className="Page">
-            <article className="PageContent">
-              <p>blah blah blah</p>
-            </article>
-          </section>
+        <div className="AppContainer">
+            <nav className="NavBar">
+              <ul>
+                <li>About</li>
+                <li>Gallery</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+    
+            <section className="Page">
+              <div className="Card">
+                <h2>About</h2>
+                <article className="About">
+                  <p>Ea eiusmod non incididunt do sit. Nisi nulla eiusmod fugiat exercitation cupidatat ullamco commodo. Nisi officia laborum ipsum in.</p>
+                </article>
+              </div>
+              <div className="Card">
+                <div className="logo">B.A.J.</div>
+              </div>
+            </section>
 
-          <footer className="Footer">
-            <ul>
-              <li>Facebook</li>
-              <li>InstaGram</li>
-              <li>Twitter</li>
-            </ul>
-          </footer>
-          <section className="Copy">
-            <p>&copy; 2019 GI(ant) llc.</p>
-          </section>
+            <footer className="Footer">
+              <section className="Copy">
+                <h3>&copy; 2019 Beverly Jones Designs.</h3>
+              </section>
+            </footer>
         </div>
      </div>
     );
